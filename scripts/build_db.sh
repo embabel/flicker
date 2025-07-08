3331d76f9c48aeb01549a9bfef801bd82aad7132
Rod Johnson <johnsonroda@gmail.com>
2025-07-08 16:04:32 +1000

    DB updates

---
+++ scripts/build_db.sh
@@ -0,0 +1,5 @@
+echo "Dangerous: This script will drop and recreate the database. Use with caution!"
+
+cd ..
+
+mvn clean spring-boot:run
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,10 @@
 
-    create schema movie_finder;
-
-    create table movie_finder.movie_buff_hobbies (
+    create table embabel.movie_buff_hobbies (
         hobby varchar(255),
         movie_buff_id varchar(255) not null
     );
 
-    create table movie_finder.movie_buff (
+    create table embabel.movie_buff (
         about varchar(255),
         country_code varchar(255),
         id varchar(255) not null,
@@ -15,29 +13,63 @@
         primary key (id)
     );
 
-    create table movie_finder.movie_buff_movie_rating (
+    create table embabel.movie_buff_movie_ratings (
         movie_buff_id varchar(255) not null,
         movie_ratings_id varchar(255) not null unique
     );
 
-    create table movie_finder.movie_rating (
+    create table embabel.movie_guide (
+        id varchar(255) not null,
+        name varchar(255),
+        objective varchar(255),
+        persona varchar(255),
+        role varchar(255),
+        voice varchar(255),
+        primary key (id)
+    );
+
+    create table embabel.movie_rating (
         rating integer not null,
         id varchar(255) not null,
         title varchar(255),
         primary key (id)
     );
 
-    alter table if exists movie_finder.movie_buff_hobbies 
+    alter table if exists embabel.movie_buff_hobbies 
        add constraint FKj7wdgfh27njulqcag0eqwq374 
        foreign key (movie_buff_id) 
-       references movie_finder.movie_buff;
+       references embabel.movie_buff;
 
-    alter table if exists movie_finder.movie_buff_movie_rating 
-       add constraint FK5axeq8jjujsgtb2b8qa9fvwtw 
+    alter table if exists embabel.movie_buff_movie_ratings 
+       add constraint FK8pwc1lpa173nok67hmybt3du 
        foreign key (movie_ratings_id) 
-       references movie_finder.movie_rating;
+       references embabel.movie_rating;
 
-    alter table if exists movie_finder.movie_buff_movie_rating 
-       add constraint FKsvgif3lsiyiupvc1bojf2rrk9 
+    alter table if exists embabel.movie_buff_movie_ratings 
+       add constraint FKo77n808aqc7xb2nu0ij97c0xt 
        foreign key (movie_buff_id) 
-       references movie_finder.movie_buff;
+       references embabel.movie_buff;
+create table flicker.movie_buff_hobbies (hobby varchar(255), movie_buff_id varchar(255) not null);
+create table flicker.movie_buff (about varchar(255), country_code varchar(255), id varchar(255) not null, name varchar(255), streaming_services varchar(255) array, primary key (id));
+create table flicker.movie_buff_movie_ratings (movie_buff_id varchar(255) not null, movie_ratings_id varchar(255) not null unique);
+create table flicker.movie_guide (id varchar(255) not null, name varchar(255), objective varchar(255), persona varchar(255), role varchar(255), voice varchar(255), primary key (id));
+create table flicker.movie_rating (rating integer not null, id varchar(255) not null, title varchar(255), primary key (id));
+alter table if exists flicker.movie_buff_hobbies add constraint FKj7wdgfh27njulqcag0eqwq374 foreign key (movie_buff_id) references flicker.movie_buff;
+alter table if exists flicker.movie_buff_movie_ratings add constraint FK8pwc1lpa173nok67hmybt3du foreign key (movie_ratings_id) references flicker.movie_rating;
+alter table if exists flicker.movie_buff_movie_ratings add constraint FKo77n808aqc7xb2nu0ij97c0xt foreign key (movie_buff_id) references flicker.movie_buff;
+create table flicker.movie_buff_hobbies (hobby varchar(255), movie_buff_id varchar(255) not null);
+create table flicker.movie_buff (about varchar(255), country_code varchar(255), id varchar(255) not null, name varchar(255), streaming_services varchar(255) array, primary key (id));
+create table flicker.movie_buff_movie_ratings (movie_buff_id varchar(255) not null, movie_ratings_id varchar(255) not null unique);
+create table flicker.movie_guide (id varchar(255) not null, name varchar(255), objective varchar(255), persona varchar(255), role varchar(255), voice varchar(255), primary key (id));
+create table flicker.movie_rating (rating integer not null, id varchar(255) not null, title varchar(255), primary key (id));
+alter table if exists flicker.movie_buff_hobbies add constraint FKj7wdgfh27njulqcag0eqwq374 foreign key (movie_buff_id) references flicker.movie_buff;
+alter table if exists flicker.movie_buff_movie_ratings add constraint FK8pwc1lpa173nok67hmybt3du foreign key (movie_ratings_id) references flicker.movie_rating;
+alter table if exists flicker.movie_buff_movie_ratings add constraint FKo77n808aqc7xb2nu0ij97c0xt foreign key (movie_buff_id) references flicker.movie_buff;
+create table flicker.movie_buff_hobbies (hobby varchar(255), movie_buff_id varchar(255) not null);
+create table flicker.movie_buff (about varchar(255), country_code varchar(255), id varchar(255) not null, name varchar(255), streaming_services varchar(255) array, primary key (id));
+create table flicker.movie_buff_movie_ratings (movie_buff_id varchar(255) not null, movie_ratings_id varchar(255) not null unique);
+create table flicker.movie_guide (id varchar(255) not null, name varchar(255), objective varchar(255), persona varchar(255), role varchar(255), voice varchar(255), primary key (id));
+create table flicker.movie_rating (rating integer not null, id varchar(255) not null, title varchar(255), primary key (id));
+alter table if exists flicker.movie_buff_hobbies add constraint FKj7wdgfh27njulqcag0eqwq374 foreign key (movie_buff_id) references flicker.movie_buff;
+alter table if exists flicker.movie_buff_movie_ratings add constraint FK8pwc1lpa173nok67hmybt3du foreign key (movie_ratings_id) references flicker.movie_rating;
+alter table if exists flicker.movie_buff_movie_ratings add constraint FKo77n808aqc7xb2nu0ij97c0xt foreign key (movie_buff_id) references flicker.movie_buff;
